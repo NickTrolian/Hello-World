336ff19c3a09b1600b5435076a4c802005ae3ab2
NickTrolian <42821549+NickTrolian@users.noreply.github.com>
2018-08-30 00:23:55 -0500

    HelloWorld.java
    Adding java file for Lab 1 assignment.

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package helloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Nick Trolian - dat33@students.uwf.edu");
+		for(int i = 1; i <= 100; ++i)
+		{
+			System.out.print(i + " ");
+		}
+
+	}
+
+}
